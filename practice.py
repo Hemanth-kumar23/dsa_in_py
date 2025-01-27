1b8521f3ab26418d80fbae842faee1dce987da43
Hemanth-kumar23 <hemantkumarr365@gmail.com>
2025-01-27 22:31:32 +0530

    lin and binary added

--- practice.py
+++ practice.py
@@ -34,3 +34,20 @@
 
 # # Output the sorted array
 # print(array)
+
+
+def lineraSerach(list, n, key):
+    for i in range (0, n):
+        if(list[i] == key):
+            return i
+    return -1
+    
+list = [1, 3, 5, 4, 7, 9]
+key = 3
+
+n = len(list)
+res = lineraSerach(list ,n , key)
+if(res == -1):
+    print("element is not found ")
+else:
+    print("Element found at index :",res)
