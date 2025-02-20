7da0d9fd91957453efce973689ac9cf2585dbf56
Hemanth-kumar23 <hemantkumarr365@gmail.com>
2025-02-20 22:19:26 +0530

    insert at begining

---
+++ insertll.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.ref = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_ll(self):
+        if self.head is None:
+            print("linkedList is empty ")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data, end=" -> ")  # Print node data
+                n = n.ref
+            print("None")
+
+    def add_begn(self,data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+
+ll1 = LinkedList()
+ll1.add_begn(10)
+ll1.print_ll()
