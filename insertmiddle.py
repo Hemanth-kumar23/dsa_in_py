636798906c6d2760662d0cb8628314f78b1b3272
Hemanth-kumar23 <hemantkumarr365@gmail.com>
2025-02-22 22:26:56 +0530

    added in middle

---
+++ insertmiddle.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.ref = None
+
+class linkedList :
+
+    def __init__(self):
+        self.head = None
+
+    def print_ll(self):
+        if self.head is None:
+            print("linked list is emty")
+        else:
+            n = self.head 
+            while n is not None:
+                print(n.data)
+                n = n.ref
+    
+    def add_beg(self,data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+
+    def add_end(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n is not None :
+                n = n.ref
+    
+    def add_after(self,data,x):
+        n = self.head
+        while n is not None :
+          if n.data == x:
+              new_node = Node(data)
+              new_node.ref = n.ref
+              n.ref = new_node
+              return
+          n = n.ref
+
+          
+               
+
+ll1=linkedList()
+ll1.add_beg(100)
+ll1.add_after(200,100)
+ll1.print_ll()
